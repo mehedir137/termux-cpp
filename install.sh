3bf0eb42e731e7b0ead68de3c1189ee942be9dd5
mehedir137 <mehedi.r137@gmail.com>
2022-01-31 21:56:16 +0600

    Added installer

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo 'Installing packages...'
+apt update && apt install clang wget curl git neovim zsh nodejs -y
+
+echo 'Changeing shell to zsh...'
+chsh -s zsh
+
+echo 'Linking vim to neovim'
+ln -s '/data/data/com.termux/files/usr/bin/nvim' '/data/data/com.termux/files/usr/bin/vim'
+
+cd '/data/data/com.termux/'
+
+echo 'Downloading configurations...'
+curl 'https://github.com/mehedir137/termux-cpp/raw/main/termux.tar.gz' -o 'termux.tar.gz'
+
+echo 'Extracting configurations...'
+tar xzf 'termux.tar.gz'
+rm 'termux.tar.gz'
+
+echo 'All done! Restart termux!'
+
